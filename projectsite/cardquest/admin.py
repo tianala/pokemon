64ecb4cce5b30f3a74d3b24fddafaee8a77cff6a
Christian Ala <202280241@psu.palawan.edu.ph>
2023-12-12 15:38:51 +0800

    modification 6.0

--- projectsite/cardquest/admin.py
+++ projectsite/cardquest/admin.py
@@ -1,24 +1,24 @@
 from django.contrib import admin
 from .models import Trainer, PokemonCard, Collection
 
-
-
 @admin.register(Trainer)
 class TrainerAdmin(admin.ModelAdmin):
-    list_display = ('created_at', 'updated_at', 'name', 'birthdate', 'location', 'email',)
-    search_fields = ('name', 'location')
+    list_display = ('name', 'birthdate', 'location', 'email', 'created_at', 'updated_at')
 
-
-# Register your models here.
+    search_fields = ('name', 'location', 'email')
 
 @admin.register(PokemonCard)
-class PokemonCardAdmin(admin.ModelAdmin):
-    list_display = ('created_at', 'updated_at', 'name', 'rarity', 'hp', 'card_type', 'attack',
-                    'description', 'weakness', 'evolution_stage', 'abilities', 'card_number', 'release_date')
-    search_fields = ('name', 'rarity', 'hp', 'card_type', 'attack',
-                     'description', 'weakness', 'evolution_stage', 'abilities','card_number', 'release_date')
-
+class PokemonAdmin(admin.ModelAdmin):
+    list_display = ('name', 'rarity','hp', 'card_type',
+                    'attack', 'description', 'weakness',
+                    'card_number', 'release_date', 'evolution_stage', 
+                    'abilities', 'created_at', 'updated_at')
+    search_fields = ('name','rarity','hp', 'card_type',
+                    'attack', 'description', 'weakness',
+                    'card_number', 'release_date', 'evolution_stage', 
+                    'abilities')
+    
 @admin.register(Collection)
 class CollectionAdmin(admin.ModelAdmin):
-    list_display = ('created_at', 'updated_at', 'trainer', 'card')
+    list_display = ('trainer', 'card', 'collection_date', 'created_at', 'updated_at')
     search_fields = ('trainer', 'card')
