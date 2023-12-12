5553712eaa980a7ee5566f2fee7615f8501f5dfe
Christian Ala <202280241@psu.palawan.edu.ph>
2023-12-12 14:11:43 +0800

    modification 4.0

--- projectsite/cardquest/admin.py
+++ projectsite/cardquest/admin.py
@@ -1,18 +1,24 @@
 from django.contrib import admin
 from .models import Trainer, PokemonCard, Collection
 
+
+
 @admin.register(Trainer)
 class TrainerAdmin(admin.ModelAdmin):
-    list_display = ('name', 'birthdate', 'location', 'email')
-
+    list_display = ('created_at', 'updated_at', 'name', 'birthdate', 'location', 'email',)
     search_fields = ('name', 'location')
 
+
+# Register your models here.
+
 @admin.register(PokemonCard)
-class PokemonAdmin(admin.ModelAdmin):
-    list_display = ('name', 'rarity')
-    search_fields = ('name',)
+class PokemonCardAdmin(admin.ModelAdmin):
+    list_display = ('created_at', 'updated_at', 'name', 'rarity', 'hp', 'card_type', 'attack',
+                    'description', 'weakness', 'evolution_stage', 'abilities', 'card_number', 'release_date')
+    search_fields = ('name', 'rarity', 'hp', 'card_type', 'attack',
+                     'description', 'weakness', 'evolution_stage', 'abilities','card_number', 'release_date')
 
 @admin.register(Collection)
 class CollectionAdmin(admin.ModelAdmin):
-    list_display = ('trainer', 'card', 'created_at', 'updated_at')
+    list_display = ('created_at', 'updated_at', 'trainer', 'card')
     search_fields = ('trainer', 'card')
